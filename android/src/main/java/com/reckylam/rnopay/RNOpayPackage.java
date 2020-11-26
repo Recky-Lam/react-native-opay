5b42cd38d44d78ae5228394afc92d81bae96d36c
Recky-Lin <reckylam@icloud.com>
2020-11-26 12:04:05 +0800

    fixed some bug in as

--- android/src/main/java/com/reckylam/rnopay/RNOpayPackage.java
+++ android/src/main/java/com/reckylam/rnopay/RNOpayPackage.java
@@ -1,6 +1,7 @@
 
 package com.reckylam.rnopay;
 
+import com.opay
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -10,6 +11,7 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 import com.facebook.react.bridge.JavaScriptModule;
+
 public class RNOpayPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
@@ -21,11 +23,6 @@ public class RNOpayPackage implements ReactPackage {
       return Collections.emptyList();
     }
 
-//    @Override
-//    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-//      return Collections.emptyList();
-//    }
-
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
@@ -34,15 +31,16 @@ public class RNOpayPackage implements ReactPackage {
     }
 
     @ReactMethod
-    public void initTransaction(String reference, String key) {
-
+    public void initTransaction(String reference, String amount, String pubKey) {
+        int amount = Integer.parseInt(amount);
         OrderInfo info = new OrderInfo();
-        info.setAmount(10);
-        info.setCurrency("NG");
-        info.setMerchantName("merchantName");
+        info.setAmount(amount);
+        info.setCurrency("NGN");
+        info.setMerchantName("Opay");
         info.setMerchantUserId("123456");
         info.setReference(reference);
         info.setPublicKey(key);
+        info.setTokenize(true)
         new PayTask(info).pay(context: MainActivity.this,
             new PayResultCallback() {
                 @Override
@@ -50,5 +48,10 @@ public class RNOpayPackage implements ReactPackage {
 
                 }
             })
+    }
+
+    @ReactMethod
+    public String version() {
+        return 'Opay SDK Version 1.2.0';
     }
 }
