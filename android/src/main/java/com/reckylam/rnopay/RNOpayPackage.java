7347fa98a6df2c2e6a2d53436d157f7f958358b7
Recky-Lin <reckylam@icloud.com>
2020-11-26 15:39:14 +0800

    fixed some bug in as gradle

--- android/src/main/java/com/reckylam/rnopay/RNOpayPackage.java
+++ android/src/main/java/com/reckylam/rnopay/RNOpayPackage.java
@@ -1,12 +1,15 @@
 
 package com.reckylam.rnopay;
 
-import com.opay
+import com.opay.account.controller.PayResultActivity;
+
+
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
